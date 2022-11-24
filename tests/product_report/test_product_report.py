a365cfef491b00125a725193a55289135a013034
mandioquynha <machado.amandag@gmail.com>
2022-11-24 17:50:34 -0300

    style: erros de lint

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -11,11 +11,11 @@ def test_relatorio_produto():
         '3690',
         'em ambiente arejado'
     )
-
-    message = ( f'O produto {product.nome_do_produto}'
+    message = (
+        f'O produto {product.nome_do_produto}'
         f' fabricado em {product.data_de_fabricacao}'
         f' por {product.nome_da_empresa}'
         f' com validade até {product.data_de_validade}'
-        f' precisa ser armazenado {product.instrucoes_de_armazenamento}.')
-    
+        f' precisa ser armazenado {product.instrucoes_de_armazenamento}.'
+    )
     assert product.__repr__() == message
