6b8c066ffa76f71d462a5cd7f549ade80d8d5e5c
mandioquynha <machado.amandag@gmail.com>
2022-11-24 14:25:59 -0300

    feat: :sparkles: teste para classe Product
    requisito 1 concluido

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
-'...'
+from inventory_report.inventory.product import Product
+
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Sonho de valsa',
+        'Lacta',
+        '08/03/2022',
+        '08/12/2022',
+        '3690',
+        'Ambiente com clima fresco'
+    )
+    assert product.id == 1
+    assert product.nome_do_produto == 'Sonho de valsa'
+    assert product.nome_da_empresa == 'Lacta'
+    assert product.data_de_fabricacao == '08/03/2022'
+    assert product.data_de_validade == '08/12/2022'
+    assert product.numero_de_serie == '3690'
+    assert product.instrucoes_de_armazenamento == 'Ambiente com clima fresco'
