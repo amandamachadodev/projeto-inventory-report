66a355832799f4ad09a9b51b81bf94fe970d8176
mandioquynha <machado.amandag@gmail.com>
2022-11-24 14:49:43 -0300

    feat: :sparkles: teste do recurso __repr__
    requisito 2 concluido

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Sonho de valsa',
+        'Lacta',
+        '08/03/2022',
+        '08/12/2022',
+        '3690',
+        'em ambiente arejado'
+    )
+
+    message = ( f'O produto {product.nome_do_produto}'
+        f' fabricado em {product.data_de_fabricacao}'
+        f' por {product.nome_da_empresa}'
+        f' com validade até {product.data_de_validade}'
+        f' precisa ser armazenado {product.instrucoes_de_armazenamento}.')
+    
+    assert product.__repr__() == message
