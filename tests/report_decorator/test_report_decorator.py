040b4d84be837dcd3d43d6d1616a67b0a3f38943
mandioquynha <machado.amandag@gmail.com>
2022-11-27 04:17:06 -0300

    test: :test_tube: implementado teste ColoredReport
    requisito 9 concluido

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,22 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+def test_decorarate_relatorio():
+    product = [{
+        "id": 1,
+        "nome_do_produto": "Sonho de valsa",
+        "nome_da_empresa": "Lacta",
+        "data_de_fabricacao": "08/03/2022",
+        "data_de_validade": "08/12/2022",
+        "numero_de_serie": "3690",
+        "instrucoes_de_armazenamento": "Ambiente com clima fresco"
+    }]
+    colored = ColoredReport(SimpleReport).generate(product)
+
+    expect = (
+        f"\033[32mData de fabricação mais antiga:\033[0m \033[36m08/03/2022\033[On\n"
+        f"\033[32mData de validade mais próxima:\033[0m \033[36m08/12/2022\033[0n\n"
+        f"\033[32mEmpresa com mais produtos:\033[0m \033[31mLacta\033[0m"
+        )
+    assert colored == expect
