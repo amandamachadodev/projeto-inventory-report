e6e6a785dcbecdfeeb20a0f0517eaec355c49e7f
mandioquynha <machado.amandag@gmail.com>
2022-11-27 04:35:20 -0300

    fix:

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -15,17 +15,20 @@ def test_decorarate_relatorio():
 
     colored = ColoredReport(SimpleReport).generate(product)
 
-    fabricacao = "\033[32mData de fabricação mais antiga:\033[0m"
-    + "\033[36m08/03/2022\033[On\n"
-    validade = "\033[32mData de validade mais próxima:\033[0m"
-    + "\033[36m08/12/2022\033[0n\n"
-    produtos = "\033[32mEmpresa com mais produtos:\033[0m"
-    + "\033[31mLacta\033[0m"
+    fabricacao = [
+        "\033[32mData de fabricação mais antiga:\033[0m",
+        "\033[36m08/03/2022\033[On\n"]
+    validade = [
+        "\033[32mData de validade mais próxima:\033[0m",
+        "\033[36m08/12/2022\033[0n\n"]
+    produtos = [
+        "\033[32mEmpresa com mais produtos:\033[0m",
+        "\033[31mLacta\033[0m"]
 
     expect = (
-        f"{fabricacao}\n"
-        f"{validade}\n"
-        f"{produtos}"
+        f"{fabricacao[0] fabricacao[1]}\n"
+        f"{validade[0] validade[1]}\n"
+        f"{produtos[0] produtos[1]}"
         )
 
     assert colored == expect
